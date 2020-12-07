250b86c078643df9e7dbd8d4282ba34df034e953
Sergey <seregasergo1654@yandex.ru>
2020-12-07 20:15:42 +0500

    correction of comments

--- index.js/index.js
+++ index.js/index.js
@@ -1,38 +1,32 @@
-const headerButtonNode = document.querySelector('.header__name-edit');
+const profileButtonNode = document.querySelector('.profile__name-edit');
 const popupNode = document.querySelector('.popup');
-const popupCloseNode = document.querySelector('.popup__close')
+const popupCloseNode = document.querySelector('.popup__close');
 
-const headerUserNameNode = document.querySelector('.header__user-name');
-const headerHobbyNode = document.querySelector('.header__hobby');
-const popupFormNode = document.querySelector('.popup__form')
-const popupInputNameNode = document.querySelector('.popup__input-name');
-const popupInputHobbyNode = document.querySelector('.popup__input-hobby');
-const popupButtonNode = document.querySelector('.popup__button');
+const profileUserNameNode = document.querySelector('.profile__user-name');
+const profileHobbyNode = document.querySelector('.profile__hobby');
+const popupFormNode = document.querySelector('.popup__form');
+const popupInputNameNode = document.querySelector('.popup__input_type_name');
+const popupInputHobbyNode = document.querySelector('.popup__input_type_hobby');
 
-headerButtonNode.addEventListener('click',handleHeaderButtonClick);
-popupCloseNode.addEventListener('click',handlePopupCloseClick);
-popupButtonNode.addEventListener('click',handlePopupCloseClick);
 
-function handleHeaderButtonClick() {
+function handleProfileButtonClick() {
     popupNode.classList.add('popup_visible');
-    popupInputHobbyNode.value = headerHobbyNode.textContent;
-    popupInputNameNode.value = headerUserNameNode.textContent;
-}
+    popupInputHobbyNode.value = profileHobbyNode.textContent;
+    popupInputNameNode.value = profileUserNameNode.textContent;
+};
 
-function handlePopupCloseClick() {
-    popupNode.classList.remove('popup_visible');
-    popupButtonNode.classList.remove('popup_visible');
-}
-
-popupFormNode.addEventListener('submit',hendleFormSubmit);
-headerHobbyNode.addEventListener('submit',hendleFormSubmit);
 
 function hendleFormSubmit(event) {
     event.preventDefault();
-    const popupInputNameNode = event.currentTarget.querySelector('.popup__input-name');
-    headerUserNameNode.textContent = popupInputNameNode.value;
+    profileHobbyNode.textContent = popupInputHobbyNode.value;
+    profileUserNameNode.textContent = popupInputNameNode.value;
+    handlePopupCloseClick().classList.remove('popup_visible');
 
-    event.preventDefault();
-    const popupInputHobbyNode = event.currentTarget.querySelector('.popup__input-hobby');
-    headerHobbyNode.textContent = popupInputHobbyNode.value;
-}
+};
+function handlePopupCloseClick() {
+    popupNode.classList.remove('popup_visible');
+};
+
+popupCloseNode.addEventListener('click',handlePopupCloseClick);
+profileButtonNode.addEventListener('click',handleProfileButtonClick);
+popupFormNode.addEventListener('submit',hendleFormSubmit);
